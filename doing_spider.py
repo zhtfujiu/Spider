a11e2f7fda0acc18edcd821c170fd2180c3927d5
fujiu <zhaohaotianzht@163.com>
2017-06-11 21:30:27 +0800

    修改了爬虫页面的显示框。单行显示。

--- doing_spider.py
+++ doing_spider.py
@@ -21,16 +21,6 @@ class Doing_Spider(object):
         self.urls = url_manager.UrlManager()
         self.fielt_urls = set()
 
-        # =========准备多线程  3个线程  3个URL管理器  3个downloader  ========
-        self.urls_1 = url_manager.UrlManager()
-        self.urls_2 = url_manager.UrlManager()
-        self.urls_3 = url_manager.UrlManager()
-
-        self.downloader_1 = html_downloader.HtmlDownloader()
-        self.downloader_2 = html_downloader.HtmlDownloader()
-        self.downloader_3 = html_downloader.HtmlDownloader()
-
-
         self.downloader = html_downloader.HtmlDownloader()
         self.outputer = html_outputer.HtmlOutputer()
         self.parser = html_parser.HtmlParser()
@@ -62,9 +52,6 @@ class Doing_Spider(object):
                 num = int(num)  #转变为int整型
                 count = 1  # 变量count记录当前爬取的是第几个URL
 
-
-                # =============这一片段内是爬取过程，等会拆分为一个线程操作 =========
-
                 self.urls.add_new_url(driver.current_url)  # 添加根URL
 
                 while self.urls.has_new_url():  # 爬虫的循环
@@ -91,17 +78,9 @@ class Doing_Spider(object):
                         count = count + 1
                     except:
                         print '本条爬取失败'
-                        self.parent.status_text.AppendText(u'本条爬取失败\n')
+                        self.parent.status_text.SetLabel(u'本条爬取失败\n')
                         self.parent.status_text.Refresh()
 
-                # ==========上面这一片段内是爬取过程，等会拆分为一个线程操作 ==========
-
-                # ========== 存储之前要把三个URL管理器的合并进一个总的URL管理器 =======
-
-
-
-
-                # ========== 存储之前要把三个URL管理器的合并进一个总的URL管理器 =======
 
 
                 try:
